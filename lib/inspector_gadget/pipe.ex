78cb915c2090c26e45931122e3cc8e3232cb9f73
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-01-02 13:20:44 +0900

    Yey event stream

--- lib/inspector_gadget/pipe.ex
+++ lib/inspector_gadget/pipe.ex
@@ -1,5 +1,4 @@
 defmodule InspectorGadget.Pipe do
-  import InspectorGadget
   import Kernel, except: [|>: 2]
 
   defmacro __using__(_) do
@@ -9,5 +8,23 @@ defmodule InspectorGadget.Pipe do
     end
   end
 
-  def a |> b, do: a Kernel.|> inspect_flow Kernel.|> b
+  defmacro left |> right do
+    function_tag =
+      case right do
+        {:&, _, [{:/, _, [{tag, _, _}]}]} -> tag
+
+        {{:., _, [{:fn, _, [{:->, _, [args | _]}]}]}, _, _} ->
+          arg_tags = Enum.map(args, fn {tag, _, _} -> Atom.to_charlist(tag) end)
+          arg_commas = Enum.intersperse(arg_tags, ', ')
+          "fn(#{Enum.join(arg_commas)})"
+
+        {tag, _ctx, _body} -> tag
+      end
+
+    quote do
+      result = Kernel.|>(unquote(left), unquote(right))
+      IO.puts("#{inspect unquote(left)} |> #{unquote(function_tag)} = #{inspect result}")
+      result
+    end
+  end
 end
